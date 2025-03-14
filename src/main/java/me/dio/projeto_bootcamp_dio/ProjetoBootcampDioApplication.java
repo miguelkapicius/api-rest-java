ac1931cd1a90d2cbb90bc787082e71ef77baa5ce
Miguel Kapicius <miguelkapicius@gmail.com>
2025-03-14 11:19:17 -0300

    Initial commit

---
+++ src/main/java/me/dio/projeto_bootcamp_dio/ProjetoBootcampDioApplication.java
@@ -0,0 +1,13 @@
+package me.dio.projeto_bootcamp_dio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoBootcampDioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoBootcampDioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/dio/projeto_bootcamp_dio/ProjetoBootcampDioApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.projeto_bootcamp_dio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoBootcampDioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
